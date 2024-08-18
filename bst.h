e8e87b1f6035771a30dd6829e1fdbcf734081713
callaghan <hattinghcallaghan@gmail.com>
2024-08-18 20:16:02 +1000

    format the files.

--- bst.h
+++ bst.h
@@ -1,35 +1,32 @@
 
-class BST
-{
+class BST {
 private:
-    struct node
-    {
-        int key;
-        node* left;
-        node* right;
-    };
+  struct node {
+    int key;
+    node *left;
+    node *right;
+  };
 
-    node* root;
+  node *root;
 
-    void AddLeafPrivate(int key, node* Ptr);
-    void PrintInOrderPrivate(node* Ptr);
-    node* ReturnNodePrivate(int key, node* Ptr);
-    int FindSmallestPrivate(node* Ptr);
-    void RemoveNodePrivate(int key, node* parent);
-    void RemoveRootMatch();
-    void RemoveMatch(node* parent, node* match, bool left);
-    node* CreateLeaf(int key);
-    node* ReturnNode(int key);
-    void RemoveSubtree(node* Ptr);
-    
+  void AddLeafPrivate(int key, node *Ptr);
+  void PrintInOrderPrivate(node *Ptr);
+  node *ReturnNodePrivate(int key, node *Ptr);
+  int FindSmallestPrivate(node *Ptr);
+  void RemoveNodePrivate(int key, node *parent);
+  void RemoveRootMatch();
+  void RemoveMatch(node *parent, node *match, bool left);
+  node *CreateLeaf(int key);
+  node *ReturnNode(int key);
+  void RemoveSubtree(node *Ptr);
+
 public:
-
-    BST();
-    ~BST();
-    void AddLeaf(int key);
-    void PrintInOrder();
-    int ReturnRootKey();
-    void PrintChildren(int key);
-    int FindSmallest();
-    void RemoveNode(int key);
+  BST();
+  ~BST();
+  void AddLeaf(int key);
+  void PrintInOrder();
+  int ReturnRootKey();
+  void PrintChildren(int key);
+  int FindSmallest();
+  void RemoveNode(int key);
 };
--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
 #include <cstdlib>
+#include <iostream>
 
 #include "bst.cpp"
 
@@ -7,43 +7,40 @@ using namespace std;
 
 int main() {
 
-    int TreeKeys[5] = {50, 76, 21, 4, 32};
-    int input = 0;
+  int TreeKeys[5] = {50, 76, 21, 4, 32};
+  int input = 0;
 
-    BST myTree;
+  BST myTree;
 
-    cout << "Print new tree: ";
+  cout << "Print new tree: ";
 
-    myTree.PrintInOrder();
+  myTree.PrintInOrder();
 
-    for(int i = 0; i < 5; i++) 
-    {
-        myTree.AddLeaf(TreeKeys[i]);
+  for (int i = 0; i < 5; i++) {
+    myTree.AddLeaf(TreeKeys[i]);
+  }
+
+  myTree.PrintInOrder();
+
+  cout << "\nDone\n";
+
+  myTree.PrintChildren(myTree.ReturnRootKey());
+
+  cout << "Smallet value:" << myTree.FindSmallest() << endl;
+
+  cout << "Enter value to delete. Enter -1 to exit.\n";
+
+  while (input != -1) {
+    cout << "\nDelete Node:";
+    cin >> input;
+    if (input == -1) {
+      cout << "exit\n";
+      return 0;
     }
-
+    myTree.RemoveNode(input);
+    cout << "\nTree:\n";
     myTree.PrintInOrder();
+  }
 
-    cout << "\nDone\n";
-
-    myTree.PrintChildren(myTree.ReturnRootKey());
-
-    cout << "Smallet value:" << myTree.FindSmallest() << endl;
-
-    cout << "Enter value to delete. Enter -1 to exit.\n";
-
-    while (input != -1)
-    {
-        cout << "\nDelete Node:";
-        cin >> input;
-        if(input == -1)
-        {
-            cout << "exit\n";
-            return 0;
-        }
-        myTree.RemoveNode(input);
-        cout << "\nTree:\n";
-        myTree.PrintInOrder();
-    }
-
-    return 0;
+  return 0;
 }
